3ce5e72aca3fab68f324910d92646d131fdb4601
Bruno Sobral <bruno.sobral.silva@gmail.com>
2018-03-13 18:48:18 -0300

    Estilizado o componente Login

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import '../css/Login.css';
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 
 export default class Login extends Component{
     constructor(){
@@ -13,7 +14,35 @@ export default class Login extends Component{
     render(){
         return(
             <div className="container-geral">
-                <h1>Login</h1>
+                <div className="box">
+                    <div className="header">
+                        <h4>Change Manager</h4>
+                    </div>
+                    <h4 className="text-center">Login</h4>                    
+                    <div className="form-body">
+                    <Form>
+                        <FormGroup row>
+                            <Label for="usuario" sm={3}>Usu&aacute;rio</Label>
+                            <Col sm={9}>
+                                <Input type="text" name="usuario" id="usuario" value={this.state.usuario} />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Label for="senha" sm={3}>Senha</Label>
+                            <Col sm={9}>
+                                <Input type="password" name="senha" id="senha" value={this.state.usuario} />
+                            </Col>
+                        </FormGroup>
+                        <div className="botoes">                            
+                             <Button block>Entrar</Button>
+                        </div>
+                        
+                    </Form>
+                    </div>
+                    <div className="footer">
+                        <div className="logo"></div>                    
+                    </div>
+                </div>
             </div>
         );
     }
